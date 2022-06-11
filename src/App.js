152361264fbc4984a48d327fcb0b9e53dbdfffcd
irvanmoses <irvanmoses10@gmail.com>
2022-06-11 22:31:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { onValue, ref, set, remove, update } from "firebase/database";
+import { db } from "./firebase";
+import { v1 as uuidv1 } from "uuid";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+  const [tempUsers, setTempUsers] = useState([]);
+
+  useEffect(() => {
+    callData();
+  }, []);
+
+  // call data to firebase realtime database
+  const callData = () => {
+    try {
+      const readData = ref(db, "users");
+      onValue(readData, (snapshot) => {
+        let records = [];
+        snapshot.forEach((child) => {
+          records.push({
+            id: child.key,
+            name: child.val().name,
+            email: child.val().email,
+            age: child.val().age,
+          });
+          setUsers(records);
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // upload data to firebase realtime database
+  const uploadData = async () => {
+    const id = uuidv1();
+    await set(ref(db, "users/" + id), {
+      id: id,
+      name: name,
+      email: email,
+      age: age,
+    });
+    setName("");
+    setEmail("");
+    setAge("");
+  };
+
+  // remove data from firebase realtime database
+  const removeData = (user) => {
+    remove(ref(db, `/users/${user}`));
+  };
+
+  // handleUpdate
+  const handleUpdate = (users) => {
+    setIsEdit(true);
+    setTempUsers(users.id);
+    setName(users.name);
+    setEmail(users.email);
+    setAge(users.age);
+  };
+
+  const submitChange = () => {
+    update(ref(db, `/users/${tempUsers}`), {
+      name: name,
+      email: email,
+      age: age,
+    });
+    setIsEdit(false);
+    setName("");
+    setEmail("");
+    setAge("");
+  };
+
+  const xButton = () => {
+    setIsEdit(false);
+    setIsEdit(false);
+    setName("");
+    setEmail("");
+    setAge("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={name}
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        value={age}
+        placeholder="Age"
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <input
+        value={email}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      {isEdit ? (
+        <>
+          <button onClick={submitChange}>Submit Change</button>
+          <button onClick={xButton}>X</button>
+        </>
+      ) : (
+        <>
+          <button onClick={uploadData}>Upload Data</button>
+        </>
+      )}
+
+      <div>
+        {users.map((user) => (
+          <ul key={user.id}>
+            <li>{user.name}</li>
+            <li>{user.email}</li>
+            <li>{user.age}</li>
+            <button onClick={() => handleUpdate(user)}>Update Data</button>
+            <button onClick={() => removeData(user.id)}>Delete Data</button>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX7VT4MZ40mEtdZkYcbgAU2wmM_6LvLV8",
+  authDomain: "react-firebase-be3e5.firebaseapp.com",
+  databaseURL:
+    "https://react-firebase-be3e5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "react-firebase-be3e5",
+  storageBucket: "react-firebase-be3e5.appspot.com",
+  messagingSenderId: "867303780668",
+  appId: "1:867303780668:web:191c73623f6d34fd146e39",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
